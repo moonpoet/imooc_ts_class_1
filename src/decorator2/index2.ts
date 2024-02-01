5ce9fc445c3847c5a468f1f3a980b13b56e0373c
陈东 <chendong@5i5j.com>
2024-02-01 15:34:35 +0800

    feat: 普通类型和值类型

--- src/decorator2/index2.ts
+++ src/decorator2/index2.ts
@@ -21,3 +21,19 @@ const person: Person = {
 
 const r = person.sayHi(person)
 console.log(r === person)
+
+// 给函数传递一个参数, 使类型不为string，而是值类型
+function foo<T>(str: T): T {
+  return str
+}
+
+// 在ts中, 注意let和const在定义变量时的类型是有区别的
+// 使用let定义
+let _r = foo('123') // string, 如果想得到123可以给泛型添加约束, T extends string
+let __r = foo('123' as const) // 123
+const ___r = foo('123') // 123
+
+// 给泛型添加约束后, 如果传入123, 会将其推导为值类型而不是string
+// 使用as const直接将其转化为值类型
+// 使用const声明变量, 这个变量的类型本身就是值类型
+
